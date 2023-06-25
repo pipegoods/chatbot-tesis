9fd981b2a3877c165ff5d29faa6c497435345e32
andres-vizcaino <andres.vizcaino@igloolab.co>
2023-06-25 11:36:14 -0500

    add analitycs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: '🤖 Chatbot Tesis',
-  description: 'Chatbot para ayudar a corregir tesis. Ejercicio practico.',
-}
+  title: "🤖 Chatbot Tesis",
+  description: "Chatbot para ayudar a corregir tesis. Ejercicio practico.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        <script
+          defer
+          src="https://unpkg.com/@tinybirdco/flock.js"
+          data-host="https://api.us-east.tinybird.co"
+          data-token="p.eyJ1IjogIjZlODNkMWUyLTY3NzMtNDk2NC1iNTExLTEyODExMDIyOWE3NSIsICJpZCI6ICIxMWZlZDdiZS1hZWRiLTRjNGYtODg4NS1lNDhjMDIyYzc2ZTcifQ.cCMR7Fgl6hhBixkZsw8Y-OUNblyLLws3Q8BQeHO7emU"
+        />
+      </body>
     </html>
-  )
+  );
 }
